7270a526bf07d4ba482c110993196b891baae5be
EzbaevRinat <dilshatakimov123@gmail.com>
2026-09-26 00:25:25 +0500

    fixed issues

--- homework-7.js
+++ homework-7.js
@@ -1,31 +1,31 @@
 function showWeather(city, temperature) {
-    console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
+console.log(`Сейчас в ${city} температура — ${temperature} градусов по Цельсию`);
 }
 showWeather("Москва", 20);
 
 
-const speedOfLight = 299792458;
+const SPEED_OF_LIGHT = 299792458;
 function checkSpeed(speed) {
-    if (speed > speedOfLight) {
-        console.log("Сверхсветовая скорость");
-    } else if (speed < speedOfLight) {
-        console.log("Субсветовая скорость");
-    } else {
-        console.log("Скорость света");
-    }
+if (speed > speedOfLight) {
+console.log("Сверхсветовая скорость");
+} else if (speed < speedOfLight) {
+console.log("Субсветовая скорость");
+} else {
+console.log("Скорость света");
+}
 }
 
 
 let product = "Шоколад";
 let price = 1000;
 function buy(budget = 700) {
-    console.log(budget);
+console.log(budget);
 if (budget > price) {
-    console.log(`${product} приобретён. Спасибо за покупку!`);
+console.log(`${product} приобретён. Спасибо за покупку!`);
 }
 else {
-    let difference = price - budget;
-    console.log(`Вам не хватает ${difference}$, пополните баланс`);
+let difference = price - budget;
+console.log(`Вам не хватает ${difference}$, пополните баланс`);
 }}
 buy(700);
 
@@ -33,11 +33,11 @@ buy(700);
 let name = "Али"
 let age = 24
 function calculateAge(name, age){
-    console.log (`${name}, вам  ${age} года`);
+console.log (`${name}, вам  ${age} года`);
 }
 calculateAge(name, age)
 
 
-let NameWater = "barjomi"
-const HEIGHT_MOUNTAIN = 8848;
-let NameStar = "sun";
+let nameWater = "barjomi"
+let heightMountain = 8848;
+let nameStar = "sun";
